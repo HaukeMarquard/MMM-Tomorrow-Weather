14a1b59e99829d9f6d55ec25a3d8048f052254db
HaukeMarquard <120113101+HaukeMarquard@users.noreply.github.com>
2023-09-17 17:46:43 +0200

    .

--- MMM-Tomorrow-Weather.js
+++ MMM-Tomorrow-Weather.js
@@ -36,63 +36,65 @@ Module.register("MMM-Tomorrow-Weather", {
             return wrapper;
         } else {
             var container = document.createElement("div");
-            var wrapper = document.createElement("div");
-            wrapper.style.display = "flex";
-            for(let i = 0; i < 4; i++) {
-                var wrapper_hourly = document.createElement("div");
-                wrapper_hourly.style.display = "flex";
-                wrapper_hourly.style.flexDirection = "column";
-                wrapper_hourly.style.justifyContent = "center";
-                wrapper_hourly.style.alignItems = "center";
-                wrapper_hourly.style.border = "1px solid white";
-                wrapper_hourly.style.borderRadius = "10px";
-                wrapper_hourly.style.padding = "3px";
-                wrapper_hourly.style.gap = "3px";
-                const p = document.createElement("p")
-                const time = new Date(this.weather.forecast.forecastday[0].hour[3*i].time)
-                // p.innerText = `${time.toLocaleTimeString()}`
-                p.innerText = `Hauke`
-                const temperature = document.createElement("p")
-                temperature.innerText = `Temp: ${this.weather.forecast.forecastday[0].hour[3*i].temp_c}`
-                const uvIndex = document.createElement("p")
-                uvIndex.innerText = `UV-Index: ${Math.round(this.weather.forecast.forecastday[0].hour[3*i].uv)}`
-                const rainIntensity = document.createElement("p")
-                rainIntensity.innerText = `Regenchance: ${this.weather.forecast.forecastday[0].hour[3*i].chance_of_rain}`
-                wrapper_hourly.appendChild(p)
-                wrapper_hourly.appendChild(temperature)
-                wrapper_hourly.appendChild(uvIndex)
-                wrapper_hourly.appendChild(rainIntensity)
-                wrapper.appendChild(wrapper_hourly)
-            }
-            container.appendChild(wrapper)
-            wrapper_daylies = document.createElement("div");
-            wrapper_daylies.style.display = "flex";
-            for(let i = 0; i < 3; i++) {
-                var wrapper_daily = document.createElement("div");
-                wrapper_daily.style.display = "flex";
-                wrapper_daily.style.flexDirection = "column";
-                wrapper_daily.style.justifyContent = "center";
-                wrapper_daily.style.alignItems = "center";
-                wrapper_daily.style.border = "1px solid white";
-                wrapper_daily.style.borderRadius = "10px";
-                wrapper_daily.style.padding = "3px";
-                wrapper_daily.style.gap = "3px";
-                const p = document.createElement("p")
-                const time = new Date(this.weather.timelines.daily[i].time)
-                p.innerText = `${time.toLocaleDateString()}`
-                const temperature = document.createElement("p")
-                temperature.innerText = `Maxtemp: ${this.weather.forecast.forecastday[i+1].day.maxtemp_c}`
-                const uvIndex = document.createElement("p")
-                uvIndex.innerText = `UV-Index: ${this.weather.forecast.forecastday[i+1].day.uv}`
-                const rainIntensity = document.createElement("p")
-                rainIntensity.innerText = `Regenmengechance: ${this.weather.forecast.forecastday[i+1].day.daily_chance_of_rain}`
-                wrapper_daily.appendChild(p)
-                wrapper_daily.appendChild(temperature)
-                wrapper_daily.appendChild(uvIndex)
-                wrapper_daily.appendChild(rainIntensity)
-                wrapper_daylies.appendChild(wrapper_daily)
-            }
-            container.appendChild(wrapper_daylies)
+            container.innerHTML = "Haukes dingens"
+            // var wrapper = document.createElement("div");
+            // wrapper.style.display = "flex";
+            // for(let i = 0; i < 4; i++) {
+            //     var wrapper_hourly = document.createElement("div");
+            //     wrapper_hourly.style.display = "flex";
+            //     wrapper_hourly.style.flexDirection = "column";
+            //     wrapper_hourly.style.justifyContent = "center";
+            //     wrapper_hourly.style.alignItems = "center";
+            //     wrapper_hourly.style.border = "1px solid white";
+            //     wrapper_hourly.style.borderRadius = "10px";
+            //     wrapper_hourly.style.padding = "3px";
+            //     wrapper_hourly.style.gap = "3px";
+            //     const p = document.createElement("p")
+            //     const time = new Date(this.weather.forecast.forecastday[0].hour[3*i].time)
+            //     // p.innerText = `${time.toLocaleTimeString()}`
+            //     p.innerText = `Hauke`
+            //     const temperature = document.createElement("p")
+            //     temperature.innerText = `Temp: ${this.weather.forecast.forecastday[0].hour[3*i].temp_c}`
+            //     const uvIndex = document.createElement("p")
+            //     uvIndex.innerText = `UV-Index: ${Math.round(this.weather.forecast.forecastday[0].hour[3*i].uv)}`
+            //     const rainIntensity = document.createElement("p")
+            //     rainIntensity.innerText = `Regenchance: ${this.weather.forecast.forecastday[0].hour[3*i].chance_of_rain}`
+            //     wrapper_hourly.appendChild(p)
+            //     wrapper_hourly.appendChild(temperature)
+            //     wrapper_hourly.appendChild(uvIndex)
+            //     wrapper_hourly.appendChild(rainIntensity)
+            //     wrapper.appendChild(wrapper_hourly)
+            // }
+            // container.appendChild(wrapper)
+            // wrapper_daylies = document.createElement("div");
+            // wrapper_daylies.style.display = "flex";
+            // for(let i = 0; i < 3; i++) {
+            //     var wrapper_daily = document.createElement("div");
+            //     wrapper_daily.style.display = "flex";
+            //     wrapper_daily.style.flexDirection = "column";
+            //     wrapper_daily.style.justifyContent = "center";
+            //     wrapper_daily.style.alignItems = "center";
+            //     wrapper_daily.style.border = "1px solid white";
+            //     wrapper_daily.style.borderRadius = "10px";
+            //     wrapper_daily.style.padding = "3px";
+            //     wrapper_daily.style.gap = "3px";
+            //     const p = document.createElement("p")
+            //     const time = new Date(this.weather.timelines.daily[i].time)
+            //     p.innerText = `${time.toLocaleDateString()}`
+            //     const temperature = document.createElement("p")
+            //     temperature.innerText = `Maxtemp: ${this.weather.forecast.forecastday[i+1].day.maxtemp_c}`
+            //     const uvIndex = document.createElement("p")
+            //     uvIndex.innerText = `UV-Index: ${this.weather.forecast.forecastday[i+1].day.uv}`
+            //     const rainIntensity = document.createElement("p")
+            //     rainIntensity.innerText = `Regenmengechance: ${this.weather.forecast.forecastday[i+1].day.daily_chance_of_rain}`
+            //     wrapper_daily.appendChild(p)
+            //     wrapper_daily.appendChild(temperature)
+            //     wrapper_daily.appendChild(uvIndex)
+            //     wrapper_daily.appendChild(rainIntensity)
+            //     wrapper_daylies.appendChild(wrapper_daily)
+            // }
+            // container.appendChild(wrapper_daylies)
+
             
             return container;
         }
