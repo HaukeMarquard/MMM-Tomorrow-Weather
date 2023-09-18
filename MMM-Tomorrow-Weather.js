59f8838ced89ceac2839506ab62432c6650ff08a
HaukeMarquard <120113101+HaukeMarquard@users.noreply.github.com>
2023-09-18 20:01:53 +0200

    .

--- MMM-Tomorrow-Weather.js
+++ MMM-Tomorrow-Weather.js
@@ -101,45 +101,45 @@ Module.register("MMM-Tomorrow-Weather", {
                 container.appendChild(wrapper)
             }
 
-            var daily_container = document.createElement("div");
-            daily_container.classList.add("daily_forecast_container");
-            for (let i = 1; i < 3; i++) {
-                var wrapper = document.createElement("div");
-                wrapper.classList.add("daily_forecast")
-                const header = document.createElement("p")
-                header.classList.add("header")
-                header.innerText = i === 1 ? "Morgen" : "Übermorgen"
-                const first_container = document.createElement("div")
-                first_container.classList.add("first")
-                const first = document.createElement("img")
-                first.src = `https:${this.weather.forecast.forecastday[i].day.condition.icon}`
-                first_container.appendChild(first)
-                const second = document.createElement("div")
-                second.classList.add("second")
-                const temp = document.createElement("p")
-                temp.innerText = `${this.weather.forecast.forecastday[i].day.mintemp_c} - ${this.weather.forecast.forecastday[i].day.maxtemp_c} °C`
-                const uv = document.createElement("p")
-                uv.innerText = `UV: ${Math.round(this.weather.forecast.forecastday[i].day.uv)}`
-                const rain_container = document.createElement("div")
-                rain_container.style.display = "flex"
-                rain_container.style.align_items = "center"
-                rain_container.style.justifyContent = "center"
-                const rain = document.createElement("p")
-                rain.innerText = `${this.weather.forecast.forecastday[i].day.daily_chance_of_rain}%`
-                const r_img = document.createElement("img")
-                r_img.classList.add("small")
-                r_img.src = "/MMM-Tomorrow-Weather/regentropfen.png"
-                rain_container.appendChild(r_img)
-                rain_container.appendChild(rain)
-                second.appendChild(temp)
-                second.appendChild(uv)
-                second.appendChild(rain_container)
-                wrapper.appendChild(header)
-                wrapper.appendChild(first_container)
-                wrapper.appendChild(second)
-                daily_container.appendChild(wrapper)
-            }
-            container.appendChild(daily_container)            
+            // var daily_container = document.createElement("div");
+            // daily_container.classList.add("daily_forecast_container");
+            // for (let i = 1; i < 3; i++) {
+            //     var wrapper = document.createElement("div");
+            //     wrapper.classList.add("daily_forecast")
+            //     const header = document.createElement("p")
+            //     header.classList.add("header")
+            //     header.innerText = i === 1 ? "Morgen" : "Übermorgen"
+            //     const first_container = document.createElement("div")
+            //     first_container.classList.add("first")
+            //     const first = document.createElement("img")
+            //     first.src = `https:${this.weather.forecast.forecastday[i].day.condition.icon}`
+            //     first_container.appendChild(first)
+            //     const second = document.createElement("div")
+            //     second.classList.add("second")
+            //     const temp = document.createElement("p")
+            //     temp.innerText = `${this.weather.forecast.forecastday[i].day.mintemp_c} - ${this.weather.forecast.forecastday[i].day.maxtemp_c} °C`
+            //     const uv = document.createElement("p")
+            //     uv.innerText = `UV: ${Math.round(this.weather.forecast.forecastday[i].day.uv)}`
+            //     const rain_container = document.createElement("div")
+            //     rain_container.style.display = "flex"
+            //     rain_container.style.align_items = "center"
+            //     rain_container.style.justifyContent = "center"
+            //     const rain = document.createElement("p")
+            //     rain.innerText = `${this.weather.forecast.forecastday[i].day.daily_chance_of_rain}%`
+            //     const r_img = document.createElement("img")
+            //     r_img.classList.add("small")
+            //     r_img.src = "/MMM-Tomorrow-Weather/regentropfen.png"
+            //     rain_container.appendChild(r_img)
+            //     rain_container.appendChild(rain)
+            //     second.appendChild(temp)
+            //     second.appendChild(uv)
+            //     second.appendChild(rain_container)
+            //     wrapper.appendChild(header)
+            //     wrapper.appendChild(first_container)
+            //     wrapper.appendChild(second)
+            //     daily_container.appendChild(wrapper)
+            // }
+            // container.appendChild(daily_container)            
             return container;
         }
     },
